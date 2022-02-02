bbf42d27acfd74ce39dfd706c2547b9e3bbc745a
Josh Devine <joshuaddevine@gmail.com>
2022-02-02 16:16:14 -0500

    overhaul to handle more servers more efficiently

--- UVAuth.py
+++ UVAuth.py
@@ -1,48 +1,62 @@
-'''
-TODO:   
-        implement file logging
-        add check for nonexistent roster files
-        add check for double-enrollment of students
-'''
+#!/usr/bin/env python3
 
 import json
-from datetime import datetime
-
+from os.path import exists
 import discord
 from discord.ext import commands
 
-CONFIG_PATH = "./config.json"
+
+COURSE_PATH = "./courses.json"
 TOKEN_PATH = "./TOKEN"
 
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+PRONOUNS = {
+    "❤️": "they/them",
+    "💛": "he/him",
+    "💚": "any pronouns",
+    "🧡": "she/her",
+    "💙": "just my name",
+    "💜": "please ask"
+}
 
-def get_from_config(attr):
-    with open(CONFIG_PATH) as f:
-        return json.load(f)[attr]
+bot = commands.Bot(
+    command_prefix="!", 
+    description="",
+    activity="DM me your computing id to be verified!",
+    intents=discord.Intents.all())
 
-def log(message):
-    if get_from_config("logging")["console"]:
-        print(f'[{datetime.now()}] {message}')
-    
-    if get_from_config("logging")['file']:
-        #TODO implement
-        pass
+
+def load_course(guild_name):
+    with open(COURSE_PATH) as f:
+        return json.load(f)[guild_name]
+
+
+def load_roster(course):
+    if exists(course["roster_path"]):
+        with open(course["roster_path"]) as f:
+            return json.load(f)
+    else:
+        print(f'ERROR: roster file for {course["course_name"]} does not exist')
+
 
 @bot.event
 async def on_ready():
-    log("UVAuth is online!")
+    print("UVAuth is online!")
+
 
 @bot.event
 async def on_member_join(member):
     '''
     dm students for verification
     '''
-    log(f"{member} joined {member.guild.name}")
+    print(f"{member} joined {member.guild.name}")
     
     unverified = discord.utils.get(member.guild.roles, name="Unverified")
 
     await member.add_roles(unverified)
-    await member.send(f"Hello there! You've joined {member.guild.name}. Please reply with your computing ID so that you can be verified.")
+    await member.send(
+        f"Hello there! You've joined {member.guild.name}. "
+        "Please reply with your computing ID so that you can be verified.")
+
 
 @bot.event
 async def on_message(message):
@@ -52,47 +66,84 @@ async def on_message(message):
     '''
     await bot.process_commands(message)
 
-    user = message.author
-    if not isinstance(message.channel, discord.channel.DMChannel) or user == bot.user:
+    if not isinstance(message.channel, discord.channel.DMChannel):
         return
 
-    log(f"received a DM from {user}")
+    user = message.author
+    if user == bot.user:
+        return
+
+    print(f"received a DM from {user}")
+
     computing_id = message.content.lower()
 
-    for course in get_from_config("courses"):
-        for guild in bot.guilds:
-            if guild.name == course["server_title"]:
-                break
+    for guild in bot.guilds:
+        course = load_course(guild.name)
+        user_member = guild.get_member(user.id)
+        if course is None or user_member is None:
+            continue
+
+        roster = load_roster()
+        if roster is None:
+            # TODO tell student that roster not implemented
+            continue
+
+        if computing_id not in roster:
+            print(f"{user} provided an invalid computing id")
+            await message.channel.send(
+                'Sorry, you either entered an invalid computing id, or you are '
+                f'not on the class roster for {course["server_title"]}! Please '
+                'try again.')
+            await message.channel.send(
+                'If your id was correct, you may need to be added to the class '
+                f'roster. In that case, please email {course["support_email"]} '
+                'to request access.')
+            continue
+
+        student = roster[computing_id]
+
+        if "student" not in student["role"].lower():
+            print(f"{user} tried to access a non-student role")
+            await message.channel.send(
+                'You provided the computing id of a staff member involved with '
+                f'{course["server_title"]}.  If this is correct, please email '
+                f'{course["support_email"]} to be verified manually.  '
+                'Otherwise, please try again.')
+            continue
 
         for member in guild.members:
-            if member != user: continue
-
-            with open(course["roster_path"]) as f:
-                students = json.load(f)
-
-            if computing_id not in students.keys():
-                log(f"{user} provided an invalid computing id")
-                await message.channel.send('Sorry, you either entered an invalid computing id, or you are currently not on the class roster! Please try again.')
-                await message.channel.send(f'If your id was correct, you may need to be added to the class roster. In that case, please email {course["support_email"]} to request access.')
-                continue
-
-            student = students[computing_id]
-
-            if "student" not in student["role"].lower():
-                log(f"{user} tried to access a non-Student role")
-                await message.channel.send(f"You provided the computing id of a staff member involved with {course['server_title']}.  If this is correct, please email {course['support_email']} to be verified manually.  Otherwise, please try again.")
-                continue
-
-            log(f"removing Unverified role and adding computing id {student['id']} to user {user}")
+            if member.nick is None: 
+                member.nick = member.name
+            if computing_id in member.nick.lower():
+                print(f'{user} tried to enter the verified id {computing_id}')
+                await message.channel.send(
+                    'Sorry, the computing id you entered has already been ',
+                    f'verified to a Discord user in {course["server_title"]}.'
+                )
+                await message.channel.send(
+                    'If you did not previously link this computing id or you '
+                    'wish to switch which Discord user is verifed to your id, '
+                    f'please email {course["support_email"]} for help.'
+        print(
+            f'removing Unverified role and adding computing id {computing_id} '
+            'to user {user}')
             
-            nickname = student["name"] + ' (' + student["id"] + ')'
-            if len(nickname) > 32: nickname = student["name"].split()[0] + ' (' + student["id"] + ')'
-            if len(nickname) > 32: nickname = student["name"].split()[0][0] + '. (' + student["id"] + ')'
-            
-            await member.edit(nick=nickname, roles=[])
-            await message.channel.send(f'Welcome to {course["server_title"]}! You should now have access to all of the student channels in the course server. If you have any questions, send a message in "#💬general". Pay attention to "#📣announcements" for important course announcements.')
-            await message.channel.send('If you would like to specify your pronouns, please refer to #pronouns for more.')
-            continue
+        nickname = f'{student["name"]} ({computing_id})'
+        if len(nickname) > 32: 
+            nickname = f'{student["name"].split()[0]} ({computing_id})'
+        if len(nickname) > 32: 
+            nickname = f'{student["name"].split()[0][0]} ({computing_id})'
+        
+        await user_member.edit(nick=nickname, roles=[])
+        await message.channel.send(
+            f'Welcome to {course["server_title"]}! You should now have access '
+            'to all of the student channels in the course server. If you have ''
+            'any questions, send a message in "#💬general". Pay attention to '
+            '"#📣announcements" for important course announcements.')
+        await message.channel.send(
+            'If you would like to specify your pronouns, please refer to '
+            '"#pronouns "for more.')
+
 
 @bot.event
 async def on_raw_reaction_add(payload):
@@ -102,24 +153,23 @@ async def on_raw_reaction_add(payload):
     guild = bot.get_guild(payload.guild_id)
     channel = guild.get_channel(payload.channel_id)
 
-    if channel.name == "pronouns":
-        member = guild.get_member(payload.user_id)
-        guild_roles = await guild.fetch_roles()
-        reaction = str(payload.emoji)
+    if channel.name != "pronouns":
+        return
 
-        if reaction == "❤️":   await member.add_roles(discord.utils.get(guild_roles,name="they/them"))
-        elif reaction == "🧡": await member.add_roles(discord.utils.get(guild_roles,name="she/her"))
-        elif reaction == "💛": await member.add_roles(discord.utils.get(guild_roles,name="he/him"))
-        elif reaction == "💚": await member.add_roles(discord.utils.get(guild_roles,name="any pronouns"))
-        elif reaction == "💙": await member.add_roles(discord.utils.get(guild_roles,name="just my name"))
-        elif reaction == "💜": await member.add_roles(discord.utils.get(guild_roles,name="please ask"))
-        
-        else:
-            message = await channel.fetch_message(payload.message_id)
-            await message.remove_reaction(payload.emoji, member)
-            return
-        
-        log(f"gave the pronoun role associated with {reaction} to {member}")
+    member = guild.get_member(payload.user_id)
+    guild_roles = await guild.fetch_roles()
+    reaction = str(payload.emoji)
+
+    if reaction in PRONOUNS:
+        pronoun_role = discord.utils.get(guild_roles,name=PRONOUNS[reaction])
+        await member.add_roles(pronoun_role)
+    else:
+        message = await channel.fetch_message(payload.message_id)
+        await message.remove_reaction(payload.emoji, member)
+        return
+    
+    print(f"gave the pronoun role associated with {reaction} to {member}")
+
 
 @bot.event
 async def on_raw_reaction_remove(payload):
@@ -128,72 +178,85 @@ async def on_raw_reaction_remove(payload):
     '''
     guild = bot.get_guild(payload.guild_id)
     channel = guild.get_channel(payload.channel_id)
-    if channel.name == "pronouns":
-        member = guild.get_member(payload.user_id)
-        guild_roles = await guild.fetch_roles()
-        reaction = str(payload.emoji)
+    
+    if channel.name != "pronouns":
+        return
+    
+    member = guild.get_member(payload.user_id)
+    guild_roles = await guild.fetch_roles()
+    reaction = str(payload.emoji)
 
-        if reaction == "❤️":   await member.remove_roles(discord.utils.get(guild_roles,name="they/them"))
-        elif reaction == "🧡": await member.remove_roles(discord.utils.get(guild_roles,name="she/her"))
-        elif reaction == "💛": await member.remove_roles(discord.utils.get(guild_roles,name="he/him"))
-        elif reaction == "💚": await member.remove_roles(discord.utils.get(guild_roles,name="any pronouns"))
-        elif reaction == "💙": await member.remove_roles(discord.utils.get(guild_roles,name="just my name"))
-        elif reaction == "💜": await member.remove_roles(discord.utils.get(guild_roles,name="please ask"))
+    if reaction in PRONOUNS:
+        pronoun_role = discord.utils.get(guild_roles,name=PRONOUNS[reaction])
+        await member.remove_roles(pronoun_role)
 
-        log(f"removed the pronoun role associated with {reaction} to {member}")
+    print(f"removed the pronoun role associated with {reaction} to {member}")
+
 
 @bot.command()
 @commands.has_role("Admin")
 async def say(ctx, arg):
     await ctx.send(arg)
 
+
 @bot.command()
 @commands.has_role("Admin")
 async def react(ctx, message_id, *emojis):
+    print()
     message = await ctx.channel.fetch_message(message_id)
     for emoji in emojis:
         await message.add_reaction(emoji)
 
+
 @bot.command()
 @commands.has_role("Admin")
 async def ping(ctx):
-    log(f"{ctx.author} sent a ping!")
+    print(f"{ctx.author} sent a ping!")
     await ctx.send("ping!")
 
-@bot.command()
-@commands.has_any_role("Admin", "Professor", "Staff")
-async def get_unverified(ctx):
-    log(f"{ctx.author} called get_unverified")
 
-    for course in get_from_config("courses"):
-        if course["server_title"] == ctx.guild.name:
-            with open(course["roster_path"]) as f:
-                students = json.load(f)
+@bot.command()
+@commands.has_role("Admin")
+async def get_unverified(ctx):
+    print(f"{ctx.author} called get_unverified")
+
+    course = load_course(ctx.guild.name)
+    if course is None:
+        # TODO print that this course not registered in courses
+        return
+
+    roster = load_roster(course)
+    if roster is None:
+        return
 
     staff = discord.utils.get(ctx.guild.roles, name="Staff")
     unverified = discord.utils.get(ctx.guild.roles, name="Unverified")
-    unverified_ids = []
 
-    for comp_id in students.keys():
-        found = False
-        if "student" not in students[comp_id]["role"].lower(): continue
+    unverified_ids = []
+    for comp_id in roster.keys():
+        if "student" not in roster[comp_id]["role"].lower(): continue
         
         for member in ctx.guild.members:
-            if staff in member.roles or unverified in member.roles: continue
-            if not member.nick: member.nick = member.name
-            if comp_id in member.nick: found = True
-
-        if not found: unverified_ids.append(comp_id)
+            if staff in member.roles or unverified in member.roles: 
+                continue
+            if member.nick is None: 
+                member.nick = member.name
+            if comp_id in member.nick:
+                unverified_ids.append(comp_id)
+                break
 
     if unverified_ids:
         unverified_ids.sort()
-        log(f"here are the unverified students: {unverified_ids}")
+        print(f"here are the unverified students: {unverified_ids}")
         await ctx.send(f"here are the unverified students: {unverified_ids}")
     else:
-        log(f"all the students are verified!")
+        print(f"all the students are verified!")
         await ctx.send(f"all the students are verified!")
+
 
 if __name__ == "__main__":
     # begin event loop using token
-    with open(TOKEN_PATH) as TOKEN:
-        bot.run(TOKEN.read())
+    with open(TOKEN_PATH) as file:
+        token = file.read()
+
+    bot.run(token)
