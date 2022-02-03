3579fe66c1ec2c536512f1194ae9ca78f1991462
Marcus Mann <mpm7cf@virginia.edu>
2022-02-03 15:41:04 -0500

    Added basic testing, setup.py for dependencies/administration

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='uva-discord-auth',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/jdvne/uva-discord-auth',
+    license='',
+    author='jdvne',
+    author_email='TODO: @jdvne add your email here',
+    description='An authentication bot used across multiple UVA CS classes',
+    install_requires=['discord.py', 'pytest']
+)
